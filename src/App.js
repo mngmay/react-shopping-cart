aa90e31a3e98b2187ccfa0a94d12f0559adb0eaa
May Ng <mngmay@gmail.com>
2019-08-12 12:58:30 -0700

    Wrapped components in ProductContext.Provider and refactored routes. Step 2 complete

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import Product from "./components/Product";
 
 function App() {
   const [products] = useState(data);
@@ -17,18 +18,14 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Navigation cart={cart} />
-
-      {/* Routes */}
-      <Route
-        exact
-        path="/"
-        render={() => <Products products={products} addItem={addItem} />}
-      />
-
-      <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
-    </div>
+    <ProductContext.Provider value={{ products, addItem }}>
+      <div className="App">
+        <Navigation cart={cart} />
+        {/* Routes */}
+        <Route exact path="/" component={Products} />} />
+        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+      </div>
+    </ProductContext.Provider>
   );
 }
 
